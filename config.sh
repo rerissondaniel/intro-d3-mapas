126c182e32cdb678736f5fac45cf339bb6c9f0e9
RERISSON DANIEL COSTA SILVA MATOS 116110612 <rerissondaniel@gmail.com>
2018-02-15 10:53:00 -0300

    Finalizar parte 1 lab03

---
+++ config.sh
@@ -0,0 +1,9 @@
+npm install d3
+npm install d3-scale-chromatic
+npm install d3-geo-projection
+sudo npm install -g shapefile
+npm install ndjson-cli
+npm install d3-dsv
+npm install topojson
+
+export PATH=$PATH:./node_modules/.bin/
--- respostas.sh
+++ respostas.sh
@@ -2,8 +2,6 @@ curl -O https://raw.githubusercontent.com/nazareno/intro-d3-mapas/master/dados/p
 
 unzip pb_setores_censitarios.zip -d pb_setores_censitarios
 
-sudo npm install -g shapefile
-
 shp2json pb_setores_censitarios/25SEE250GC_SIR.shp -o pb.json
 
 du -hsm pb_setores_censitarios/25SEE250GC_SIR.shp
@@ -12,16 +10,12 @@ du -hsm pb_setores_censitarios/25SEE250GC_SIR.shp
 du -hsm pb.json
 #11 MB
 
-npm install -g d3-geo-projection
-
 geoproject \
   'd3.geoOrthographic().rotate([54, 14, -2]).fitSize([1000, 600], d)' \
   < pb.json \
   > pb-ortho.json
 
 geo2svg -w 1000 -h 600 < pb-ortho.json > pb-ortho.svg
-
-npm install -g ndjson-cli
 
 ndjson-split 'd.features' < pb-ortho.json > pb.ndjson
 
@@ -31,8 +25,6 @@ curl -O https://raw.githubusercontent.com/nazareno/intro-d3-mapas/master/dados/P
 
 unzip PB_20171016.zip
 
-npm install d3-dsv
-
 dsv2json -r ';' -n PB/Base\ informaçoes\ setores2010\ universo\ PB/CSV/Pessoa10_PB.csv
 
 ndjson-map 'd.Cod_setor = d.properties.CD_GEOCODI, d' < pb.ndjson > pb-censo-ortho-sector.ndjson
@@ -41,17 +33,15 @@ ndjson-join 'd.Cod_setor'  pb-censo-ortho-sector.ndjson pb-censo.ndjson > pb-map
 
 ndjson-map 'd[0].properties = {porcent_com_registro: d[1].V001 * 100 / (d[1].V001 + d[1].V002 + d[1].V003) * 100}' < pb-mapa-com-dados.ndjson > pb-ortho-com-registro.ndjson
 
-npm install topojson
-
 geo2topo -n tract=pb-ortho-com-registro.ndjson > pb-tracts-topo.json
 
-toposimplify -p 1 -f < pb-tracts-topo.json | topoquantize 1e5 > pb-quatized-topo.json
+toposimplify -p 1 -f < pb-tracts-topo.json | topoquantize 1e5 > pb-quantized-topo.json
 
-npm install d3
-npm install d3-scale-chromatic
-
-topo2geo tracts=- < pb-quantized-topo.json | ndjson-map -r d3 'z = d3.scaleSequentia (d3.interpolateViridis).domain([0, 1e3]), d.features.forEach(f => f.properties.fill = z(f.properties.porcent_com_registro)), d' | ndjson-split 'd.features'  | geo2svg -n --stroke none -w 1000 -h 600  > pb-tracts-threshold-light.svg
-
-./node_modules/.bin/topo2geo tracts=- < pb-quantized-topo.json | ./node_modules/.bin/ndjson-map -r d3 'z = d3.scaleSequentia (d3.interpolateViridis).domain([0, 1e3]), d.features.forEach(f => f.properties.fill = z(f.properties.porcent_com_registro)), d' | ./node_modules/.bin/ndjson-split 'd.features'  | ./node_modules/.bin/geo2svg -n --stroke none -w 1000 -h 600  > pb-tracts-threshold-light.svg
-
+topo2geo tracts=- \
+  < pb-quantized-topo.json \
+  | ndjson-map -r d3 \
+  'z = d3.scaleSequential(d3.interpolateViridis).domain([0, 1e3]), d.features.forEach(f => f.properties.fill = z(f.properties.porcent_com_registro)), d' \
+  | ndjson-split 'd.features' \
+  | geo2svg -n --stroke none -w 1000 -h 600 \
+  > pb-tracts-threshold-light.svg
 
