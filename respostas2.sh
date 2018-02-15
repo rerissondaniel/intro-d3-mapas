43bfaf005fc6dc9f443c3492f173ced118e2544c
RERISSON DANIEL COSTA SILVA MATOS 116110612 <rerissondaniel@gmail.com>
2018-02-15 12:06:40 -0300

    Iniciada a parte 2

--- respostas2.sh
+++ respostas2.sh
@@ -1,6 +1,6 @@
 topo2geo tracts=- \
   < pb-quantized-topo.json \
   | ndjson-map -r d3 -r d3=d3-scale-chromatic\
-  'z = d3.scaleThreshold().domain([0, 50, 90, 95, 98, 100]).range(d3.schemeReds[5]), d.features.forEach(f => f.properties.fill = z(f.properties.porcent_com_registro)), d' \
+  'z = d3.scaleThreshold().domain([99, 99.2, 99.5, 99.7, 100]).range(d3.schemeBuGn[4]), d.features.forEach(f => f.properties.fill = z(f.properties.porcent_com_registro)), d' \
   | ndjson-split 'd.features' \
   | geo2svg -n --stroke none -w 1000 -h 600 \
